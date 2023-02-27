4c3f06d63760f06fa4616d9c9d5853562e06547f
Osman Goni <osmancse32@gmail.com>
2023-02-27 19:40:39 +0600

    without mime-type

--- demo_file_system.js
+++ demo_file_system.js
@@ -2,55 +2,78 @@ const http = require('http');
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
-const mime = require('mime-types');
 
 const port = 8080;
 
 const server = http.createServer(function (req, res) {
-    if (req.method === "GET") {
-        const parsedUrl = url.parse(req.url);
-        const hostName = parsedUrl.hostname || req.headers.host;
-        const reqPath = parsedUrl.pathname;
+  if (req.method === 'GET') {
+    const parsedUrl = url.parse(req.url);
+    const reqPath = parsedUrl.pathname;
 
-        const myDirectory = __dirname;
-        const filePath = path.join(myDirectory, reqPath);
+    // Get the file extension from the requested path
+    const fileExtension = path.extname(reqPath);
 
-        console.log(filePath);
+    // Set the default content type
+    let contentType = 'text/plain';
 
-        fs.access(filePath, fs.constants.R_OK, (err) => {
-            if (err) {
-              res.statusCode = 404;
-              res.end('File not found');
-              server.close();
-              return;
-            }
-
-            const stat = fs.statSync(filePath);
-            const contentType = mime.contentType(path.extname(filePath));
-
-            res.writeHead(200, {
-              'Content-Type': contentType,
-              'Content-Length': stat.size,
-            });
-
-            const stream = fs.createReadStream(filePath);
-            stream.on('error', (err) => {
-              console.error(err);
-              res.statusCode = 500;
-              res.end('Internal server error');
-            });
-
-            stream.pipe(res);
-        
-            // logFile.write(`${new Date()} - ${req.method} ${req.url} - ${res.statusCode}\n`);
-        
-          });
-
-        // res.statusCode = 200;
-        // res.setHeader('Content-Type', 'text/plain');
-        // res.end(`Host Name: ${hostName} Requested URL: ${req.url}\nDirectory: ${myDirectory}\nPath: ${reqPath}\nFile Path: ${filePath} `);
-        // console.log("GET Method Requested");
-
-        server.close();
+    // Set the appropriate content type based on the file extension
+    switch (fileExtension) {
+      case '.html':
+        contentType = 'text/html';
+        break;
+      case '.css':
+        contentType = 'text/css';
+        break;
+      case '.js':
+        contentType = 'text/javascript';
+        break;
+      case '.jpeg':
+      case '.jpg':
+        contentType = 'image/jpeg';
+        break;
+      case '.png':
+        contentType = 'image/png';
+        break;
+      case '.gif':
+        contentType = 'image/gif';
+        break;
     }
-}).listen(port);
+
+    // Get the file path relative to the server root directory
+    const filePath = path.join(__dirname, reqPath);
+
+    // Check if the file exists and is readable
+    fs.access(filePath, fs.constants.R_OK, (err) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end('File not found');
+        return;
+      }
+
+      // Check if the file extension matches the content type
+      if (!contentType.startsWith('image') && !contentType.startsWith('text')) {
+        res.statusCode = 415;
+        res.end('Unsupported media type');
+        return;
+      }
+
+      // Get the file stats to set the content length
+      const stat = fs.statSync(filePath);
+
+      // Set the response headers
+      res.writeHead(200, {
+        'Content-Type': contentType,
+        'Content-Length': stat.size,
+      });
+
+      // Create a read stream for the file and pipe it to the response
+      const stream = fs.createReadStream(filePath);
+      stream.pipe(res);
+    });
+  }
+  server.close();
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
